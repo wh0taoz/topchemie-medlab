dcc89ae36fce61996370ebab34ddf23c433f963a
wh0taoz <simona.mecava@gmail.com>
2025-09-17 00:00:39 +0200

    dodate stranice i sadrzaj

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-import Home from './pages/Home';
-import About from './pages/About';
+import ONama from './pages/ONama';
+import Medicina from './pages/proizvodi/Medicina';
+import FarmakoIndustrija from './pages/proizvodi/FarmakoIndustrija';
+import EkocertDeterdzenti from './pages/proizvodi/EkocertDeterdzenti';
+import SajmoviKongresi from './pages/SajmoviKongresi';
+import Kontakt from './pages/Kontakt';
 
 function App() {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<ONama />} />
+          <Route path="/o-nama" element={<ONama />} />
+          <Route path="/proizvodi/medicina" element={<Medicina />} />
+          <Route path="/proizvodi/farmako-industrija" element={<FarmakoIndustrija />} />
+          <Route path="/proizvodi/ekocert-deterdzenti" element={<EkocertDeterdzenti />} />
+          <Route path="/sajmovi-kongresi" element={<SajmoviKongresi />} />
+          <Route path="/kontakt" element={<Kontakt />} />
         </Routes>
       </Layout>
     </Router>
